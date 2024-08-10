99f59f3ef6c96a28719e70ed81306d329c785886
earth2travis <travis.mccutcheon@gmail.com>
2024-08-10 18:05:18 -0500

    Build

---
+++ docs/dist/assets/share-g6Ri0E1Y.js
@@ -0,0 +1,74 @@
+import{u as t,j as e}from"./index-D3NueeWj.js";const o={title:"Share Retro Signal Session",description:"undefined"};function i(s){const n={a:"a",blockquote:"blockquote",code:"code",div:"div",h1:"h1",h2:"h2",h3:"h3",header:"header",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...t(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"share-retro-signal-session",children:["Share Retro Signal Session",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#share-retro-signal-session",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.p,{children:["Once the choices from Demo Day have been ",e.jsx(n.a,{href:"/procedures/seasonal-objectives/add-choices",children:"added"})," the community needs to be notified."]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Click the session on ",e.jsx(n.a,{href:"https://signal.daohaus.fun/#/0xa/0xf5d6b637a9185707f52d40d452956ca49018247a",children:"Signals"})]}),`
+`,e.jsx(n.li,{children:"Verify the choices have been added"}),`
+`,e.jsx(n.li,{children:"Click the session and copy URL"}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"shill",children:["Shill",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#shill",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.h3,{id:"post-link-in--tavern-channel-on-discord",children:["Post link in ",e.jsx(n.a,{href:"https://discord.com/channels/709210493549674598/735525646196801668",children:e.jsx(n.code,{children:"🍻-tavern"})})," channel on Discord",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#post-link-in--tavern-channel-on-discord",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"PublicHaus Season #6 Objectives are live!"}),`
+`]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://signal.daohaus.fun/",children:"https://signal.daohaus.fun/"})}),`
+`]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"The objectives have been seeded from last week's Community Camp but feel free to add choices you feel should be a priority!"}),`
+`]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"Voting ends tomorrow so vote early vote often"}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"post-link-in-forum-discussion-for-shipped-in-season-",children:["Post link in forum discussion for Shipped in Season ",e.jsx(n.code,{children:"#"}),e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#post-link-in-forum-discussion-for-shipped-in-season-",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["Season 6 objectives are live: ",e.jsx(n.a,{href:"https://signal.daohaus.fun/",children:"https://signal.daohaus.fun/"})]}),`
+`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Post link on Twitter"}),`
+`,e.jsx(n.li,{children:"Post from other social accounts"}),`
+`,e.jsxs(n.li,{children:["Post link to tweet in ",e.jsx(n.a,{href:"https://discord.com/channels/709210493549674598/758440646553370635",children:e.jsx(n.code,{children:"💥-news-flash"})})," channel on Discord"]}),`
+`,e.jsx(n.li,{children:"Encourage the community to vote, add choices, and share"}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"remind",children:["Remind",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#remind",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.p,{children:["The day before the Seasonal Objectives Session ends repeat steps in ",e.jsx(n.a,{href:"#shill",children:"Shill"})]}),`
+`,e.jsxs(n.h2,{id:"calculate-results",children:["Calculate Results",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#calculate-results",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.p,{children:["Open the ",e.jsx(n.a,{href:"https://docs.google.com/spreadsheets/d/1H3LddNt9qqzbfUJf2My6N79QCz440l653lFPcswULbA/edit?usp=sharing",children:"DAOhaus Retro Rewards 2024"})," Google Sheet. If you do not have access please request it from Ξ2T."]}),`
+`,e.jsxs(n.p,{children:["Duplicate the ",e.jsx(n.code,{children:"Season # Objectives"}),"tab and rename it according to the current season."]}),`
+`,e.jsxs(n.p,{children:["You will need to populate the data in the spreadsheet from the corresponding TCR. For Example, ",e.jsx(n.a,{href:"https://signal.daohaus.fun/#/0xa/0xf5d6b637a9185707f52d40d452956ca49018247a/tcr/0x55d8a5fb55e6f960edaeaec5d24c3424027d8051",children:"PublicHaus Season #6 Objectives"})]}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Copy the name of ",e.jsx(n.strong,{children:"Objectives"})," into Column A"]}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/total-staked.png",alt:"Total Staked"})}),`
+`,e.jsxs(n.ol,{start:"2",children:[`
+`,e.jsxs(n.li,{children:["For each objective, copy the ",e.jsx(n.strong,{children:"Total Staked"})," number in white and paste it into the spreadsheet."]}),`
+`,e.jsxs(n.li,{children:["For each objective, copy the ",e.jsx(n.strong,{children:"Quadratic Votes"})," number in red and paste it into the spreadsheet."]}),`
+`,e.jsxs(n.li,{children:["Calculate the totals for both ",e.jsx(n.strong,{children:"Total Staked"})," and ",e.jsx(n.strong,{children:"Quadratic Votes"})]}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"/images/total-tokens.png",alt:"Total"})}),`
+`,e.jsxs(n.ol,{start:"5",children:[`
+`,e.jsxs(n.li,{children:["For the ",e.jsx(n.strong,{children:"Total nvPUB"})," paste the total from the DAO Tokens section of the ",e.jsx(n.a,{href:"https://admin.daohaus.club/#/molochv3/0xa/0xf5d6b637a9185707f52d40d452956ca49018247a/settings",children:"DAO Settings"})," page."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Participation %"})," is calculated by dividing ",e.jsx(n.strong,{children:"Total Staked"})," by ",e.jsx(n.strong,{children:"Total nvPUB"}),"."]}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"share-results",children:["Share Results",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#share-results",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:"After the signal session ends post the results in Discord and Twitter."}),`
+`,e.jsxs(n.h3,{id:"post-link-in--tavern-channel-on-discord-1",children:["Post link in ",e.jsx(n.a,{href:"https://discord.com/channels/709210493549674598/735525646196801668",children:e.jsx(n.code,{children:"🍻-tavern"})})," channel on Discord",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#post-link-in--tavern-channel-on-discord-1",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:[`PublicHaus Season #6 Objectives are set!
+`,e.jsx(n.a,{href:"https://signal.daohaus.fun/",children:"https://signal.daohaus.fun/"}),`
+The community has prioritized Objective 1
+screenshot
+and also feels we should be spending time on Objective 2
+screenshot
+There was a total of 681,872.18 nvPUB in this session for a participation rate of 37%
+Appreciate everyone for participating. Let's get back to building`]}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"post-link-in-forum-discussion-for-shipped-in-season--1",children:["Post link in forum discussion for Shipped in Season ",e.jsx(n.code,{children:"#"}),e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#post-link-in-forum-discussion-for-shipped-in-season--1",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The results of the Season #6 Objectives signal session are in: ",e.jsx(n.a,{href:"https://signal.daohaus.fun/",children:"https://signal.daohaus.fun/"})]}),`
+`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Post link on Twitter"}),`
+`,e.jsx(n.li,{children:"Post from other social accounts"}),`
+`,e.jsxs(n.li,{children:["Post link to tweet in ",e.jsx(n.a,{href:"https://discord.com/channels/709210493549674598/758440646553370635",children:e.jsx(n.code,{children:"💥-news-flash"})})," channel on Discord"]}),`
+`,e.jsx(n.li,{children:"Thank the community for participating and let's get back to building"}),`
+`]})]})}function r(s={}){const{wrapper:n}={...t(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}export{r as default,o as frontmatter};
